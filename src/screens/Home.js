b55e35b4af019bb91beaff412d896f1eef509fae
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-14 12:03:24 +0900

    Authentication 예제

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,9 @@
-function Home() {
-    return <h1>홈</h1>;
-}
-export default Home;
+function Home({ setIsLoggedIn }) {
+    return (
+      <div>
+          <h1>홈</h1>
+          <button onClick={() => setIsLoggedIn(false)}>로그아웃</button>
+      </div>
+      );
+  }
+  export default Home;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,9 @@
-function Login() {
-    return <h1>로그인</h1>;
+function Login({ setIsLoggedIn }) {
+  return (
+    <div>
+        <h1>로그인</h1>
+        <button onClick={() => setIsLoggedIn(true)}>로그인</button>
+    </div>
+    );
 }
 export default Login;
