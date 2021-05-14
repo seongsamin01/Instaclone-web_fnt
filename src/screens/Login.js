c2bdcd6349811a17dfd99f363ba37d1e6c3fa6bd
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-14 13:05:48 +0900

    styled components 기본 1

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
 import { isLoggedInVar } from "./apollo";
 
+const Title = styled.h1`
+    color: ${(props) => (props.potato ? "palevioletred" : "beige")};
+    font-family: ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, 
+                Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    ${(props) =>
+       props.potato 
+            ? css`
+                font-size: 49px;
+              ` 
+            : css`
+                text-decoration: underline;
+              `}
+`;
+
+const Container = styled.div`
+    background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+    color: red;
+`;
+
 function Login() {
+    const [potato, setPotato] = useState(false);
+    const togglePotato = () => setPotato((current) => !current);
     return (
-      <div>
-          <h1>로그인</h1>
-          <button onClick={() => isLoggedInVar(true)}>로그인</button>
-      </div>
-      );
+      <Container>
+          <Title potato={potato}>로그인</Title>
+          <TogglePotato onClick={togglePotato}>Toggle On/Off</TogglePotato>
+      </Container>
+    );
   }
   export default Login;
