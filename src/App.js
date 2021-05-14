472237aa4a9f22a0b7c21ba2b995372c78ae1e5b
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-14 11:11:15 +0900

    React Router /#/HashRouter
    Router 하위개념 Route
    한번에 여러개 불러옴
    But Switch를 써주면 한번에 하나만불러옴. / 가 우선권이라서 / 붙은건 모두 /만 불러옴. 그래서 path에 exact를 붙여줘야함 그러면 오직 /일때만 불러옴 예를 들어 /photo /banana 등일 때는 안불러옴
    
    HashRouter와 browser?Router의 차이는 Hash가 배포하기 더 수월함.
    browser는 몇단계 더 거쳐야 함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/potato">
+            <h1>감자</h1>
+          </Route>
+          <Route path="/banana">
+            <h1>바나나</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
