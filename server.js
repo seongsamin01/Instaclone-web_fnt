e74bed1a71a01964162ed428c6778d784707f740
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-18 16:41:25 +0900

    Revert "dotenv 설치"
    This reverts commit b217585db2dcf193ad4fbb4657030faab80014c9.

--- server.js
+++ server.js
@@ -1,15 +1,52 @@
-require("dotenv").config();
-import { ApolloServer } from "apollo-server";
-import schema from "./schema";
+import { PrismaClient } from "@prisma/client";
+import { ApolloServer, gql } from "apollo-server";
 
+const client = new PrismaClient();
+
+const typeDefs = gql`
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { id }) => {
+      return true;
+    },
+  },
+};
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
 });
-
-const PORT = process.env.PORT;
-
 server
-  .listen(PORT)
+  .listen()
   .then(() =>
-    console.log(`💗 Server is running on http://localhost:${PORT}/ 💗`)
+    console.log(" 💗 Server is running on http://localhost:4000/ 💗 ")
   );
