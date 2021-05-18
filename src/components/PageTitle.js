9c61b1bb147fe2b3392fc17b2e490a79991444ba
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-18 14:37:45 +0900

    Apollo Client (백+프론트)

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,11 +2,15 @@ import { PropTypes } from "prop-types";
 import { Helmet } from "react-helmet-async";
 
 function PageTitle({ title }) {
-  return <Helmet>{title} | Instaclone</Helmet>;
+  return (
+    <Helmet>
+      <title>{title} | Instaclone</title>
+    </Helmet>
+  );
 }
 
 PageTitle.propTypes = {
-  title: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
 };
 
 export default PageTitle;
