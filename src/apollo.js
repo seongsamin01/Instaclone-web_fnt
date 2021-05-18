aacf1fc2afd86841d774bc305cf86d185e3823d6
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-18 16:43:02 +0900

    Revert "Apollo Client (백+프론트)"
    This reverts commit 9c61b1bb147fe2b3392fc17b2e490a79991444ba.

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,4 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import { makeVar } from "@apollo/client";
 
 export const isLoggedInVar = makeVar(false);
 export const darkModeVar = makeVar(false);
-export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-});
