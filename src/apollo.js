0b07a5477ae03dc2b464423153621c677d9bb86d
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-19 12:29:40 +0900

    Revert "Header part 3"
    This reverts commit 5845f3aa74a1ed7ce615e5bfbd73fdc7561b7b85.

--- src/apollo.js
+++ src/apollo.js
@@ -1,15 +1,9 @@
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-  makeVar,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
 const TOKEN = "TOKEN";
 const DARK_MODE = "DARK_MODE";
 
-export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN))); 
 export const logUserIn = (token) => {
   localStorage.setItem(TOKEN, token);
   isLoggedInVar(true);
@@ -18,30 +12,20 @@ export const logUserOut = () => {
   localStorage.removeItem(TOKEN);
   window.location.reload();
 };
-export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE) === "enabled"));
+
 export const enableDarkMode = () => {
   localStorage.setItem(DARK_MODE, "enabled");
   darkModeVar(true);
 };
+
 export const disableDarkMode = () => {
   localStorage.removeItem(DARK_MODE);
   darkModeVar(false);
 };
 
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      token: localStorage.getItem(TOKEN),
-    },
-  };
-});
-
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,6 +1,5 @@
 import { gql, useQuery, useReactiveVar } from "@apollo/client";
-import { useEffect } from "react";
-import { isLoggedInVar, logUserOut } from "../apollo";
+import { isLoggedInVar } from "../apollo";
 
 const ME_QUERY = gql`
   query me {
@@ -12,16 +11,11 @@ const ME_QUERY = gql`
 `;
 
 function useUser() {
-  const hasToken = useReactiveVar(isLoggedInVar);
-  const { data } = useQuery(ME_QUERY, {
-    skip: !hasToken,
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const { data, error } = useQuery(ME_QUERY, {
+    skip: !isLoggedIn,
   });
-  console.log(data);
-  useEffect(() => {
-    if (data?.me === null) {
-      logUserOut();
-    }
-  }, [data]);
+  console.log(data, error);
   return;
 }
 export default useUser;
