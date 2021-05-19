758a2a863bd3150265d776f04726433575f611e3
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-05-19 12:30:12 +0900

    Revert "Apollo Client (백엔드랑 프론트 파일은 별개임)"
    This reverts commit 9bf286ad8b8c369552a600a59973672e8fc2c71a.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { client, darkModeVar, isLoggedInVar } from "./apollo";
+import { darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
@@ -14,28 +14,26 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ApolloProvider client={client}>
-      <HelmetProvider>
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-          <GlobalStyles />
-          <Router>
-            <Switch>
-              <Route path={routes.home} exact>
-                {isLoggedIn ? <Home /> : <Login />}
+    <HelmetProvider>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <Router>
+          <Switch>
+            <Route path={routes.home} exact>
+              {isLoggedIn ? <Home /> : <Login />}
+            </Route>
+            {!isLoggedIn ? (
+              <Route path={routes.signUp}>
+                <SignUp />
               </Route>
-              {!isLoggedIn ? (
-                <Route path={routes.signUp}>
-                  <SignUp />
-                </Route>
-              ) : null}
-              <Route>
-                <NotFound />
-              </Route>
-            </Switch>
-          </Router>
-        </ThemeProvider>
-      </HelmetProvider>
-    </ApolloProvider>
+            ) : null}
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,14 +2,11 @@ import { PropTypes } from "prop-types";
 import { Helmet } from "react-helmet-async";
 
 function PageTitle({ title }) {
-  return (
-    <Helmet>
-      <title>{title} | Instaclone</title>
-    </Helmet>
-  );
+  return <Helmet>{title} | Instaclone</Helmet>;
 }
 
 PageTitle.propTypes = {
   title: PropTypes.string.isRequired,
 };
+
 export default PageTitle;
